ed4bf079b24e5aa7107700491d19048bc230cffa
Michael Nelson <absoludity@gmail.com>
2012-12-22 23:14:52 +0100

    Simple bubble-sort - 100 times faster than default sort for 1000 nums?

--- sort.go
+++ sort.go
@@ -1,5 +1,17 @@
 package sort
 
 func Ints(a []int) {
-	return
+    var sorted bool
+    for {
+        sorted = true
+        for i := 0; i < len(a)-1; i++ {
+            if a[i] > a[i+1] {
+                a[i], a[i+1] = a[i+1], a[i]
+                sorted = false
+            }
+        }
+        if sorted {
+            return
+        }
+    }
 }
