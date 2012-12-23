a5abe3aaaee181cf7ba7efd1db4ccc69da47e452
Michael Nelson <absoludity@gmail.com>
2012-12-23 14:09:34 +0100

    Simpler benchmarking with RandomIntSlice.

--- sort.go
+++ sort.go
@@ -5,20 +5,7 @@ import (
 )
 
 func Ints(a []int) {
-	var sorted bool
-	for {
-		sorted = true
-		len_minus_1 := len(a) - 1
-		for i := 0; i < len_minus_1; i++ {
-			if a[i] > a[i+1] {
-				a[i], a[i+1] = a[i+1], a[i]
-				sorted = false
-			}
-		}
-		if sorted {
-			return
-		}
-	}
+    Sort(DefaultSort.IntSlice(a))
 }
 
 func Sort(data DefaultSort.Interface) {
